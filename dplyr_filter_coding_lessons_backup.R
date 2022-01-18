90040c4f9412f1d418961275fd6b812aa9a2dae1
Andy Murtha <james.a.murtha@gmail.com>
2022-01-18 17:08:53 -0500

    Add backup for v1 of exam, revised content for final version of presentation and code along.

--- dplyr_filter_coding_lessons_backup.R
+++ dplyr_filter_coding_lessons_backup.R
@@ -14,9 +14,7 @@ view(starwars)
 
 ### Filter the Star Wars characters to Human species only
 
-
 #-----------------------------------------------------------------------------#
-
 
 # Logical Operators
 
@@ -29,34 +27,51 @@ view(starwars)
 
 ### Filter the Star Wars characters to only Human species with a sex of female
 
-
 #-----------------------------------------------------------------------------#
-
 
 # Filter Example
 
 ### Say we want to find characters that have no hair and have a black eye color
 
-
 #-----------------------------------------------------------------------------#
-
 
 # Fill in the Blank
 
 ### Question: What characters from the Star Wars movies are either species of Wookie or Twi'lek?
 
-_____(starwars, _____ == "Wookiee" _ species __ _____) %>%
+_____(starwars, _____ == "Wookie" _ species __ _____) %>%
   select(!(homeworld:starships)) %>% slice_head(n=5)
-
 
 #-----------------------------------------------------------------------------#
 
+# Additional Concepts - How to Handle NA
+
+##NA represents an unknown value so missing values are "contagious"
+##Almost any operation involving an unknown value will also be unknown
+##filter() only includes rows where the condition is TRUE; it excludes both FALSE and NA values
+##If you want to preserve missing values, ask for them explicitly
+##If you want to determine if a value is missing, use is.na()
+
+### List all Star Wars characters who do not have a listed home world or species
+
+#-----------------------------------------------------------------------------#
+
+# Additional Concepts - Filtering when Grouping
+
+##Filtering may yield different results on grouped data frames | tibbles because expressions are computed within groups
+##Make sure you are aware when applying a global filter or when filtering on groups within the data frame | tibble
+
+### The following filters rows where `mass` is greater than the global average
+
+### #This example keeps rows where the `mass` is greater than the gender average
+
+#-----------------------------------------------------------------------------#
 
 # Code if Yourself!
 
 ### Which non-Human Star Wars characters are in more than two films?
 
 
-  
-  
+
+
 
